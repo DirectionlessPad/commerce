914bac5c4ed172f1d15fb75586973129ecaea858
DirectionlessPad <patrickmoore937@gmail.com>
2024-03-04 18:35:53 +0000

    Added docstrings for models.py and views.py

--- auctions/models.py
+++ auctions/models.py
@@ -4,9 +4,33 @@ from . import categories
 
 
 class User(AbstractUser):
+    """Registers users.
+    
+    Attributes:
+    listings -- listing objects put up by this user
+    bids_placed -- bid objects placed by this user
+    watchlist -- watchlist object for listings watched by this user
+    comments_left -- comments left by this user on various listing objects
+    """
     pass
 
+
 class Listing(models.Model):
+    """Records details of each listing.
+    
+    Attributes:
+    pk -- primary key
+    title -- the name of the listing
+    description -- a description of the item in the listing
+    current_bid -- the highest bid placed on the listing so far
+    image -- url for an image (optional)
+    category -- category for the listed item (optional)
+    seller -- user who listed the item
+    closed -- boolean recording open/closed state of the listing
+    bids_received -- bid objects placed on this item
+    watched_by -- user objects who have this item on their watchlist
+    comments -- comment objects left on this item
+    """
     title = models.CharField(verbose_name="Title", max_length=64, default="New Item")
     description = models.CharField(verbose_name="Description", max_length=1500, null=True)
     current_bid = models.DecimalField(verbose_name="Starting Bid", max_digits=17, decimal_places = 2, default = 1)
@@ -17,13 +41,25 @@ class Listing(models.Model):
 
 
 class Bid(models.Model):
+    """Records details of succesful bids.
+    
+    Attributes:
+    item -- listing object the bid was placed on
+    bidder -- user object that placed the bid
+    value -- monetary value of the bid
+    """
     item = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="bids_received")
     bidder = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bids_placed", null=True, blank=True)
     value = models.DecimalField(max_digits=17, decimal_places = 2, default = 1)
 
+
 class Watchlist(models.Model):
+    """Records listings being watched by a user.
+    
+    """
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="watchlist")
     item = models.ManyToManyField(Listing, blank=True, related_name="watched_by")
+
 
 class Comment(models.Model):
     comment = models.CharField(max_length=1500, null=True)
--- auctions/views.py
+++ auctions/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 from django.contrib import messages
@@ -10,7 +10,9 @@ from . import categories
 from .my_forms import BidForm, CommentForm, CreateListingForm
 from .models import User, Listing, Bid, Watchlist, Comment
 
+
 def index(request):
+    """Display homepage with list of active listings."""
     listings = Listing.objects.filter(closed="False")
     return render(request, "auctions/index.html", {
         "listings": listings,
@@ -19,6 +21,7 @@ def index(request):
 
 
 def login_view(request):
+    """Display login page and perform login user operation."""
     if request.method == "POST":
 
         # Attempt to sign user in
@@ -40,12 +43,15 @@ def login_view(request):
     else:
         return render(request, "auctions/login.html")
 
+
 def logout_view(request):
+    """Perform logout user operation."""
     logout(request)
     return HttpResponseRedirect(reverse("index"))
 
 
 def register(request):
+    """Display register page and perform register user operation."""
     if request.method == "POST":
         username = request.POST["username"]
         email = request.POST["email"]
@@ -73,8 +79,36 @@ def register(request):
     else:
         return render(request, "auctions/register.html")
 
+
+def listing(request, listing_id: str):
+    """Display listing page.
+    
+    Arguments:
+    listing_id -- primary key for the listing
+    """
+    listing = Listing.objects.get(pk=listing_id)
+    bidform = BidForm(initial={"value": listing.current_bid})
+    if request.user.is_authenticated:
+        user_watchlist = Watchlist.objects.get(user=request.user)
+        if listing in user_watchlist.item.all():
+            in_watchlist=True
+        else:
+            in_watchlist=False
+    else:
+        in_watchlist=False
+    return render(request, "auctions/listing_page.html", {
+        "listing": listing,
+        "in_watchlist": in_watchlist,
+        "bid_form": bidform,
+        "highest_bid": Bid.objects.get(item=listing, value=listing.current_bid),
+        "comment_form": CommentForm,
+        "comments": Comment.objects.filter(item=listing),
+    })
+
+
 @login_required
 def create_listing(request):
+    """Display listing and perform create listing operations."""
     if request.method == "POST":
         if not request.user.is_authenticated:
             return HttpResponseRedirect(reverse("login"))
@@ -106,31 +140,14 @@ def create_listing(request):
         "create_form": CreateListingForm,
     })
 
-def listing(request, listing_id: str):
-    listing = Listing.objects.get(pk=listing_id)
-    bidform = BidForm(initial={"value": listing.current_bid})
-    if request.user.is_authenticated:
-        user_watchlist = Watchlist.objects.get(user=request.user)
-        if listing in user_watchlist.item.all():
-            in_watchlist=True
-        else:
-            in_watchlist=False
-    else:
-        in_watchlist=False
-    return render(request, "auctions/listing_page.html", {
-        "listing": listing,
-        "in_watchlist": in_watchlist,
-        "bid_form": bidform,
-        "highest_bid": Bid.objects.get(item=listing, value=listing.current_bid),
-        "comment_form": CommentForm,
-        "comments": Comment.objects.filter(item=listing),
-    })
 
 @login_required
 def watchlist(request):
+    """Display user watchlist page."""
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse("login"))
-    user_watchlist = request.user.watchlist.all()[0]
+    user_watchlist= Watchlist.objects.get(user=request.user)
+    # user_watchlist = request.user.watchlist.all()[0]
     watchlist_listings=[]
     for item in user_watchlist.item.all():
         watchlist_listings.append(item)
@@ -139,7 +156,9 @@ def watchlist(request):
         "heading": "Your Watchlist"
     })
 
+
 def watchlist_add(request) -> HttpResponseRedirect:
+    """Add listing to user's watchlist."""
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse("login"))
     listing_id = request.POST["listing_to_add"]
@@ -148,15 +167,23 @@ def watchlist_add(request) -> HttpResponseRedirect:
     user_watchlist.item.add(listing)
     return HttpResponseRedirect(reverse("view_listing", args=[listing.pk]))
 
+
 @login_required
 def watchlist_remove(request):
+    """Remove listing from user's watchlist."""
     listing_id = request.POST["listing_to_remove"]
     listing = Listing.objects.get(pk=listing_id)
     user_watchlist = Watchlist.objects.get(user=request.user)
     user_watchlist.item.remove(listing)
     return HttpResponseRedirect(reverse("view_listing", args=[listing.pk]))
 
+
 def new_bid(request, listing_id):
+    """Process new bids.
+    
+    Arguments:
+    listing_id -- primary key for the listing
+    """
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse("login"))
     listing = Listing.objects.get(pk=listing_id)
@@ -174,21 +201,28 @@ def new_bid(request, listing_id):
             value=bid,
         )
         new_bid.save()
+    user_watchlist= Watchlist.objects.get(user=request.user)
+    if listing not in user_watchlist:
+        user_watchlist.item.add(listing)
     return HttpResponseRedirect(reverse("view_listing", args=[listing_id]))
 
 
-
-    
-
 @login_required
 def close_auction(request):
+    """Ends the auction."""
     listing_id = request.POST["listing_to_close"]
     listing = Listing.objects.get(pk=listing_id)
     listing.closed="True"
     listing.save()
     return HttpResponseRedirect(reverse("view_listing", args=[listing.pk]))
 
+
 def add_comment(request, listing_id):
+    """Process new comments.
+    
+    Arguments:
+    listing_id -- primary key for the listing
+    """
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse("login"))
     listing = Listing.objects.get(pk=listing_id)
@@ -203,12 +237,20 @@ def add_comment(request, listing_id):
         new_comment.save()
     return HttpResponseRedirect(reverse("view_listing", args=[listing_id]))
 
+
 def select_category(request):
+    """Display category selection page."""
     return render(request, "auctions/select_category.html", {
         "categories": [category[1] for category in categories],
     })
 
+
 def category(request, category_name):
+    """Display listings that fall into a selected category.
+    
+    Arguments:
+    category_name -- selected category to view listings for
+    """
     category_listings = Listing.objects.filter(category=category_name)
     return render(request, "auctions/index.html", {
         "listings": category_listings,
